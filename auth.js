3555525f168a37cc682df807163f90f122608009
emporium8 <sharmaraghav8905@gmail.com>
2025-05-06 14:47:22 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,9 @@
+const authConfig = {
+    google: {
+        clientId: '52657429929-bs3uuieuo4pmm8kf7afo8788kbvrf68l.apps.googleusercontent.com',
+        redirectUri: 'https://yuvaworks.vercel.app/auth/callback',
+        scope: 'email profile openid'
+    }
+};
+
+module.exports = authConfig;
